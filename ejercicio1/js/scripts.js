bbd9b3efaede33e3d2e620baf827e0ae7eea2746
Camila Bellis <belliscamila@gmail.com>
2018-06-01 19:29:41 -0300

    fisrt exercise

--- ejercicio1/js/scripts.js
+++ ejercicio1/js/scripts.js
@@ -1,1 +1,27 @@
-/* here goes the scripts */
+var listItems = {
+    items: [],
+    assamble: function(item){
+        return '<div class="row">'
+        + '<span class="col-2"></span>'
+        + '<span class="col-2">' + item.brand + '</span>'
+        + '<span class="col-2">' + item.location + '</span>'
+        + '<span class="col-2">' + item.lunch + '</span>'
+        + '<span class="col-2">' + item.price + '</span>'
+        + '<span class="col-2"></span>'
+        +'<span class="description col-12"></span>'
+        + '</div>';
+    },
+
+    listAll: function(){
+        for (var i=0; i < listItems.items.length; i++){
+            listItems.assamble(listItems.items[i])
+            $('#table-body').append(
+                listItems.assamble(listItems.items[i])
+            );
+        }
+    },
+
+    setItems: function (items){
+        listItems.items = items;
+    },
+}
