19df5b6f3198ff07612efdeed1bda95a29304f75
Camila Bellis <belliscamila@gmail.com>
2018-06-15 18:41:42 -0300

    ejercicio1

--- ejercicio1/js/scripts.js
+++ ejercicio1/js/scripts.js
@@ -14,7 +14,6 @@ var listItems = {
 
     listAll: function(){
         for (var i=0; i < listItems.items.length; i++){
-            listItems.assamble(listItems.items[i])
             $('#table-body').append(
                 listItems.assamble(listItems.items[i])
             );
@@ -24,4 +23,68 @@ var listItems = {
     setItems: function (items){
         listItems.items = items;
     },
+
+    filterByBrand: function (brand){
+        for (var i=0; i < listItems.items.length; i++){
+            if (listItems.items[i].brand == brand){
+                $('#table-body').append(
+                    listItems.assamble(listItems.items[i])
+                );
+            }
+        }
+    },
+
+    filterByLocation: function (location){
+        for (var i=0; i < listItems.items.length; i++){
+            if (listItems.items[i].location == location){
+                $('#table-body').append(
+                    listItems.assamble(listItems.items[i])
+                );
+            }
+        }
+    },
+
+    filterByLunch: function (lunch){
+        for (var i=0; i < listItems.items.length; i++){
+            if (listItems.items[i].lunch == lunch){
+                $('#table-body').append(
+                    listItems.assamble(listItems.items[i])
+                );
+            }
+        }
+    },
+
+    filterByPrice: function (price){
+        for (var i=0; i < listItems.items.length; i++){
+            if (listItems.items[i].price == price){
+                $('#table-body').append(
+                    listItems.assamble(listItems.items[i])
+                );
+            }
+        }
+    }
 }
+listItems.setItems(elementslist);
+$('#brand').on('click', function(){
+    listItems.filterByBrand();
+})
+
+$('#price').on('click', function(){
+    listItems.filterByPrice();
+})
+
+$('#set').on('click',function(){
+    listItems.setItems(elementslist);
+    })
+
+$('#clear').on('click', function(){
+    listItems.clearTable();  
+    })
+    
+    $('#lunch').on('click', function(){
+        listItems.filterByLunch();
+    })
+
+    $('#location').on('click', function(){
+        listItems.filterByLocation();
+    })
